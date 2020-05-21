f633657986ad16b8411aee6166838e50173c593f
Adrian Torres <51291227+adrinho10@users.noreply.github.com>
2020-05-21 17:46:15 -0300

    Add files via upload

---
+++ Hello Github/src/br/com/adrian/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.adrian.git;
+
+/**
+ * @author Adrian
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello git");
+	}
+
+}
